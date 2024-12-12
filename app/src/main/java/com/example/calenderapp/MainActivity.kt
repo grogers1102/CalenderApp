0ab8e3b949a3ee25f0fddf6e97f795c127943c04
kenneth <kennethchau@cpp.edu>
2024-12-11 19:12:49 -0800

    appbar added

--- app/src/main/java/com/example/calenderapp/MainActivity.kt
+++ app/src/main/java/com/example/calenderapp/MainActivity.kt
@@ -2,22 +2,27 @@ package com.example.calenderapp
 
 import android.os.Bundle
 import android.text.TextUtils.replace
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.CalendarView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.commit
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.room.Room
 import com.example.calenderapp.ui.theme.CalenderAppTheme
 import com.example.calenderapp.CalendarAdapter
-import com.example.calenderapp.databinding.ActivityMainBinding
 import database.EventDao
 import database.EventDatabase
 import kotlinx.coroutines.launch
@@ -34,5 +39,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_add_event -> {
+                val navController = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+                    ?.let { androidx.navigation.fragment.NavHostFragment.findNavController(it) }
+                navController?.navigate(R.id.eventDetailFragment)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
